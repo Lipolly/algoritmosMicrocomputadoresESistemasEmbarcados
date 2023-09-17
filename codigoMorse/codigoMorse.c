08b2a9f643be277bfa57591564648bebb7b06a47
Ricardo Piero Lippoli Batista <66374159+Lipolly@users.noreply.github.com>
2023-09-17 20:48:03 -0300

    Update codigoMorse.c

--- codigoMorse/codigoMorse.c
+++ codigoMorse/codigoMorse.c
@@ -1,11 +1,11 @@
 //Ricardo Piero Lippoli Batista || GAT135
 
-void apagaLED(int miliSeg){         //apaga o led durante 0,3 segundos para espaço entre letras/numeros ou 0,9 segundos para espaço entre palavras
-     portd = 0;
-     delay_ms(miliSeg);
+void apagaLED(int miliSeg){                                    //apaga o led durante 0,3 segundos para espaço entre letras/numeros ou 0,9 segundos para espaço entre palavras
+     portd = 0;                                                //valor apagado
+     delay_ms(miliSeg);                                        //tempo fica apagado
 }
 
-void pausa(int palavraContinua){
+void pausa(int palavraContinua){                               //faz o controle de pausas entre palavras ou letras
      if(palavraContinua){
           apagaLED(300);
      }
@@ -14,66 +14,66 @@ void pausa(int palavraContinua){
      }
 }
 
-void caractere(int palavraContinua, int tipoCarac, int vezes){ //acende o led por 0,6 segundos e depois apaga, fazendo um traço
-     while(int i; i < vezes; i++){
-          portd = 255;
-          delay_ms(tipoCarac); //se tipoCarac = 200 é um ponto se = 600 é um traço
-          pausa(palavraContinua);
+void caractere(int palavraContinua, int tipoCarac, int vezes){ //pisca o led de acordo com as variaveis recebidos fazendo um ponto ou traço
+     while(int i; i < vezes; i++){                             //repete o trecho de acordo com o numero de vezes que o caracter aparece de maneira identica
+          portd = 255;                                         //valor acesso
+          delay_ms(tipoCarac);                                 //tempo que fica acesso || se tipoCarac = 200 é um ponto se = 600 é um traço
+          pausa(palavraContinua);                              //pausa entre caracteres
      }
 }
 
-void main() {          //main
+void main() {                                                  //main
      trisb = 1;
      trisd = 0;
-     apagaLED(100);       //inicia o led pagado
+     apagaLED(100);                                            //inicia o led pagado
      
      while(1){
-              while(portb.rb0 == 0){  //Se rb0 for precionado
-                    caractere(1, 200, 1);  //R
+              while(portb.rb0 == 0){                           //Se rb0 for precionado
+                    caractere(1, 200, 1);                      //R
                     caractere(1, 600, 1);
                     caractere(0, 200, 1);
                     
-                    caractere(1, 200, 1);  //P
+                    caractere(1, 200, 1);                      //P
                     caractere(1, 600, 2);
                     caractere(0, 200, 1);
                     
-                    caractere(1, 200, 1);  //L
+                    caractere(1, 200, 1);                      //L
                     caractere(1, 600, 1);
                     caractere(1, 200, 1);
                     caractere(0, 200, 1);
                     
-                    caractere(1, 600, 1);  // B
+                    caractere(1, 600, 1);                      // B
                     caractere(1, 200, 2);
                     caractere(0, 200, 1);
                     
               }
               
-              while(portb.rb1 == 0){  //Se rb1 for precionado 
-                    caractere(1, 200, 2);  //2
+              while(portb.rb1 == 0){                           //Se rb1 for precionado 
+                    caractere(1, 200, 2);                      //2
                     caractere(1, 600, 2);
                     caractere(0, 600, 1);
                    
-                    caractere(1, 600, 4);  //0
+                    caractere(1, 600, 4);                      //0
                     caractere(0, 200, 1);
                    
-                    caractere(1, 200, 2);  //2
+                    caractere(1, 200, 2);                      //2
                     caractere(1, 600, 2);
                     caractere(0, 600, 1);
                     
-                    caractere(1, 600, 4);  //0
+                    caractere(1, 600, 4);                      //0
                     caractere(0, 600, 1);
                    
-                    caractere(1, 200, 1);  //1
+                    caractere(1, 200, 1);                      //1
                     caractere(1, 600, 3);
                     caractere(0, 600, 1);
                    
-                    caractere(1, 600, 4);  //0
+                    caractere(1, 600, 4);                      //0
                     caractere(0, 600, 1);
                    
-                    caractere(1, 200, 4);  //5
+                    caractere(1, 200, 4);                      //5
                     caractere(0, 200, 1);
                    
-                    caractere(1, 600, 4);  //0
+                    caractere(1, 600, 4);                      //0
                     caractere(0, 600, 1);
                    
                     caractere(1, 200, 4);  //5
