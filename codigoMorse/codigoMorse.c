a9bf6af4805567f9890aa397800c08eee76609d4
Ricardo Piero Lippoli Batista <66374159+Lipolly@users.noreply.github.com>
2023-09-18 10:03:47 -0300

    Update codigoMorse.c

--- codigoMorse/codigoMorse.c
+++ codigoMorse/codigoMorse.c
@@ -1,32 +1,27 @@
 //Ricardo Piero Lippoli Batista || GAT135
-
-void apagaLED(int miliSeg){                                    //apaga o led durante 0,3 segundos para espaço entre letras/numeros ou 0,9 segundos para espaço entre palavras
+void apagaLED(int miliSeg){                                    //função que apaga os leds
      portd = 0;                                                //valor apagado
      delay_ms(miliSeg);                                        //tempo fica apagado
 }
-
 void pausa(int palavraContinua){                               //faz o controle de pausas entre palavras ou letras
      if(palavraContinua){
-          apagaLED(300);
+          apagaLED(300);                                       //apaga o led durante 0,3 segundos para espaço entre letras/numeros
      }
      else{
-          apagaLED(900);
+          apagaLED(900);                                       //apaga o led durante 0,9 segundos para espaço entre palavras
      }
 }
-
 void caractere(int palavraContinua, int tipoCarac, int vezes){ //pisca o led de acordo com as variaveis recebidos fazendo um ponto ou traço
-     while(int i; i < vezes; i++){                             //repete o trecho de acordo com o numero de vezes que o caracter aparece de maneira identica
+     while(int i = 0; i < vezes; i++){                         //repete o trecho com o numero de vezes que o mesmo caracter aparece
           portd = 255;                                         //valor acesso
           delay_ms(tipoCarac);                                 //tempo que fica acesso || se tipoCarac = 200 é um ponto se = 600 é um traço
           pausa(palavraContinua);                              //pausa entre caracteres
      }
 }
-
 void main() {                                                  //main
      trisb = 1;
      trisd = 0;
      apagaLED(100);                                            //inicia o led pagado
-     
      while(1){
               while(portb.rb0 == 0){                           //Se rb0 for precionado
                     caractere(1, 200, 1);                      //R
@@ -45,9 +40,7 @@ void main() {                                                  //main
                     caractere(1, 600, 1);                      // B
                     caractere(1, 200, 2);
                     caractere(0, 200, 1);
-                    
               }
-              
               while(portb.rb1 == 0){                           //Se rb1 for precionado 
                     caractere(1, 200, 2);                      //2
                     caractere(1, 600, 2);
