29744fed138573d31d9fd3a1e821f81b8f484314
Ricardo Piero Lippoli Batista <66374159+Lipolly@users.noreply.github.com>
2023-09-20 09:04:12 -0300

    Update codigoMorse.c

--- codigoMorse/codigoMorse.c
+++ codigoMorse/codigoMorse.c
@@ -2,17 +2,17 @@
 void pausa(int palavraContinua){
      portd = 0;                                                //faz o controle de pausas entre palavras ou letras
      if(palavraContinua){
-          delay_ms(300);                                       //apaga o led durante 0,3 segundos para espaço entre letras/numeros
+          delay_ms(300);                                       //apaga o led durante 0,3 segundos para espaÃ§o entre letras/numeros
      }
      else{
-          delay_ms(900);                                       //apaga o led durante 0,9 segundos para espaço entre palavras
+          delay_ms(900);                                       //apaga o led durante 0,9 segundos para espaÃ§o entre palavras
      }
 }
-void caractere(int palavraContinua, int tipoCarac, int vezes){ //pisca o led de acordo com as variaveis recebidos fazendo um ponto ou traço
+void caractere(int palavraContinua, int tipoCarac, int vezes){ //pisca o led de acordo com as variaveis recebidos fazendo um ponto ou traÃ§o
      int i = 0;
      portd = 255;
      while(i < vezes){                                         //repete o trecho com o numero de vezes que o mesmo caracter aparece
-          if(tipoCarac == 200){                                //tempo que fica acesso || se tipoCarac = 200 é um ponto se = 600 é um traço
+          if(tipoCarac == 200){                                //tempo que fica acesso || se tipoCarac = 200 Ã© um ponto se = 600 Ã© um traÃ§o
                delay_ms(200);
           }
           else{
@@ -26,7 +26,7 @@ void main() {                                                  //main
      trisb = 1;
      trisd = 0;
      while(1){
-              while(portb.rb0 == 0){                           //Se rb0 for precionado
+              if(portb.rb0 == 0){                           //Se rb0 for precionado
                     caractere(1, 200, 1);                      //R
                     caractere(1, 600, 1);
                     caractere(0, 200, 1);
@@ -44,7 +44,7 @@ void main() {                                                  //main
                     caractere(1, 200, 2);
                     caractere(0, 200, 1);
               }
-              while(portb.rb1 == 0){                           //Se rb1 for precionado
+              else if(portb.rb1 == 0){                           //Se rb1 for precionado
                     caractere(1, 200, 2);                      //2
                     caractere(1, 600, 2);
                     caractere(0, 600, 1);
